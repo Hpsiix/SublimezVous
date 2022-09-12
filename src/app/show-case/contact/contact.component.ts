1e02bbde83d8fffc31bb5f78d7196b0167902aa3
Hpsiix <djalil-normal@hotmail.fr>
2022-09-12 02:58:23 +0200

    test

---
+++ src/app/show-case/contact/contact.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() {}
+    ngOnInit(): void {}
+    display: any;
+    center: google.maps.LatLngLiteral = {
+        lat: 24,
+        lng: 12
+    };
+    zoom = 4;
+    moveMap(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.center = (event.latLng.toJSON());
+    }
+    move(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.display = event.latLng.toJSON();
+    }
+
+}
